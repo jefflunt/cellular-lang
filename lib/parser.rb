dca984e96c5aa0a8a646661692175c1fe260c4d5
Jeff Lunt <jefflunt@gmail.com>
2017-11-26 11:06:15 -0600

    Fix indentation

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,8 +1,7 @@
 class Parser
   def self.parse(cellular_text)
     parser = CellularLangParser.new
-
-		tree = parser.parse(cellular_text)
-		tree ? [tree, nil] : [nil, parser.failure_reason]
-	end
+    tree = parser.parse(cellular_text)
+    tree ? [tree, nil] : [nil, parser.failure_reason]
+  end
 end
